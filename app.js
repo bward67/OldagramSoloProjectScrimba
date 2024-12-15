24e852c9f3b6b544abf7373d7fbf0a2cb8f8647c
Barbara <subwaynaas@yahoo.ie>
2024-12-15 13:02:07 +0200

    heart icon works now

--- app.js
+++ app.js
@@ -1,40 +1,8 @@
-// import { data } from "./index.js";
+import { posts } from "./index.js";
 
-const heartIcon = document.getElementById("heart-icon");
 const mainSection = document.getElementById("main-section");
 
 let count = 0;
-
-const posts = [
-  {
-    name: "Vincent van Gogh",
-    username: "vincey1853",
-    location: "Zundert, Netherlands",
-    avatar: "images/avatar-vangogh.jpg",
-    post: "images/post-vangogh.jpg",
-    comment: "just took a few mushrooms lol",
-    likes: 21,
-  },
-  {
-    name: "Gustave Courbet",
-    username: "gus1819",
-    location: "Ornans, France",
-    avatar: "images/avatar-courbet.jpg",
-    post: "images/post-courbet.jpg",
-    comment: "i'm feelin a bit stressed tbh",
-    likes: 4,
-  },
-  {
-    name: "Joseph Ducreux",
-    username: "jd1735",
-    location: "Paris, France",
-    avatar: "images/avatar-ducreux.jpg",
-    post: "images/post-ducreux.jpg",
-    comment:
-      "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-    likes: 152,
-  },
-];
 
 for (let i = 0; i < posts.length; i++) {
   //console.log(posts[i].name);
@@ -58,7 +26,7 @@ for (let i = 0; i < posts.length; i++) {
         />
       </section> <footer>
         <div class="icons">
-          <img src="images/like.png" alt="heart icon" id="heart-icon" onclick=${addLikes}/>
+          <img src="images/like.png" alt="heart icon" id="heart-icon" />
           <img src="images/comment.png" alt="comment bubble icon" />
           <img src="images/share.png" alt="share icon" />
         </div>
@@ -66,9 +34,12 @@ for (let i = 0; i < posts.length; i++) {
         <p class="comments">${posts[i].username}<span> ${posts[i].comment}</span></p>
       </footer>
       `;
+  const heartIcon = document.getElementById("heart-icon");
 
   const likes = document.getElementById("likes");
   console.log(likes);
+
+  heartIcon.addEventListener("click", addLikes);
 
   function addLikes() {
     count++;
